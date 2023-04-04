7b3bcbe997ddba1e8662423decf2da6b50852612
Alfredo Rodriguez <fredd.073100@gmail.com>
2023-04-04 10:17:59 -0600

    FUS-3 creacion del endpoint en donde obtenemos todos los equipos sin la base de datos

--- src/controllers/liga.ts
+++ src/controllers/liga.ts
@@ -1,3 +1,11 @@
 import { Request, Response } from 'express';
 
-export const ligaController = {};
+export const ligaController = {
+  obtenerTodosLosEquipos: async (_: Request, res: Response) => {
+    try {
+      res.status(200).json({ ok: true, msg: 'Todos los equipos', status: 200 });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
--- src/routes/liga.ts
+++ src/routes/liga.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { ligaController } from '../controllers/liga';
 
 const router = Router();
+
+// http://localhost:4001/api/liga/equipos [GET]
+router.get('/equipos', ligaController.obtenerTodosLosEquipos);
 
 export default router;

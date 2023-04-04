d7fa13485d86f5571dab554f1094b791e404e2ea
Alfredo Rodriguez <fredd.073100@gmail.com>
2023-04-04 10:41:51 -0600

    Corrigiendo linea que por default el es lint corrige al guardar

--- src/routes/liga.ts
+++ src/routes/liga.ts
@@ -3,7 +3,6 @@ import { ligaController } from '../controllers/liga';
 
 const router = Router();
 
-
 // http://localhost:4001/api/liga/equipos [GET]
 router.get('/equipos', ligaController.obtenerTodosLosEquipos);
 

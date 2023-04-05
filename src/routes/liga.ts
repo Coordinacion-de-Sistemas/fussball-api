c94e524c77310e7584f23da35cbe11c0e26c6e22
Alfredo Rodriguez <fredd.073100@gmail.com>
2023-04-05 10:08:12 -0600

    Se agrega endpoint de buscar un equipo por nombre

--- src/routes/liga.ts
+++ src/routes/liga.ts
@@ -9,7 +9,7 @@ router.get('/equipos', ligaController.obtenerTodosLosEquipos);
 // http://localhost:4001/api/liga/equipo/:id [GET]
 router.get('/equipo/:id', ligaController.obtenerUnEquipoPorId);
 
-// http://localhost:4001/api/liga/equipo/:nombre [GET]
-router.get('/equipo/:id', ligaController.obtenerUnEquipoPorNombre);
+// http://localhost:4001/api/liga/equipo/nombre/:nombre [GET]
+router.get('/equipo/nombre/:nombre', ligaController.obtenerUnEquipoPorNombre);
 
 export default router;
